dfe39101b5275dc7d44e2192014ddb2c7ca36874
Erik Guenther <eguenther89@gmail.com>
2023-05-09 22:52:05 +0200

    Merge manually nmcm_test and nmcm

--- Mc_Messenger/data/js/main.js
+++ Mc_Messenger/data/js/main.js
@@ -23,7 +23,7 @@ function stringToBytes(str) {
 
 function initUIElements(){
 	viewport_check();
-	overlay();
+	overlay("login");
 }
 
 function initEventHandler(){
@@ -164,78 +164,6 @@ function addUserToUI(id, user){
 function removeUserFromUI(uid){
 	document.getElementById("ul" + uid).remove();
 }
-
-function angrymode(){
-	document.body.classList.toggle("angrymode");
-	document.getElementById("chat_box").classList.toggle("angrymode");
-	document.getElementById("msg_input").classList.toggle("angrymode");
-	document.getElementById("frame").classList.toggle("angryshake");
-}
-
-function check_usrname(){
-	const name_field = document.getElementById("user_name");
-	const name = String(document.getElementById("user_name").value);
-	if(name_field.classList.contains("shake-horizontal")){
-		name_field.classList.remove("shake-horizontal");
-		console.log("removed class");
-	}
-	if(name.includes(" ") || name == ""){
-		name_field.classList.add("shake-horizontal");
-		return false;
-	} else{
-		return true;
-	}
-}
-function overlay() {
-	var background = document.getElementById("pubg");
-	background.style.display = "flex";
-	const start_button = document.getElementById("start_button");
-	const name_field = document.getElementById("user_name");
-	start_button.onclick = function (){
-		if(check_usrname()){
-			background.style.display = "none";
-			userdb.me.na = name_field.value;
-			msghandler.sendMetaChange();
-		}
-	}
-	window.onclick = function(event) {
-		if (event.target == background && check_usrname()) {
-			background.style.display = "none";
-			userdb.me.na = name_field.value;
-			msghandler.sendMetaChange();
-		}
-	}
-}
-
-function viewport_check() {
-	console.log("Viewport_check")
-	let mobile_viewport = window.matchMedia("(max-width: 500px)");
-	let pc_viewport = window.matchMedia("(min-width: 501px");
-	function mobile_viewport_change(){
-		if (mobile_viewport.matches) {
-			console.log("mobile");
-			var fragment = document.createDocumentFragment();
-			fragment.appendChild(document.getElementById("userlist"));
-			document.getElementById("menu_files_content").appendChild(fragment);
-		}
-	}
-	mobile_viewport.addListener(mobile_viewport_change);
-
-	function pc_viewport_change(){
-		if (pc_viewport.matches) {
-			console.log("pc");
-			var fragment = document.createDocumentFragment();
-			fragment.appendChild(document.getElementById("userlist"));
-			document.getElementById("userlist_parent").appendChild(fragment);
-			expand_menu("menu_files");
-		}
-	}
-	pc_viewport.addListener(pc_viewport_change);
-
-	mobile_viewport_change();
-	pc_viewport_change();
-}
-
 
 function addMessageToChatBox(obj){
 	console.log(obj);
